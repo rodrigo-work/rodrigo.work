29da61572a61ba6fca4c21171c5366b8feb6704f
Rodrigo Ribeiro <me@rodrigo3d.com>
2024-11-06 04:04:59 -0300

    chore: add prettiere

---
+++ prettier.config.js
@@ -0,0 +1,37 @@
+/** @type {import('prettier').Config} */
+
+module.exports = {
+  arrowParens: 'always',
+  useTabs: false,
+  endOfLine: 'lf',
+  trailingComma: 'none',
+  semi: false,
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  bracketSpacing: true,
+  importOrder: [
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^types$',
+    '^@/types/(.*)$',
+    '^@/config/(.*)$',
+    '^@/lib/(.*)$',
+    '^@/hooks/(.*)$',
+    '^@/components/ui/(.*)$',
+    '^@/components/(.*)$',
+    '^@/styles/(.*)$',
+    '^@/app/(.*)$',
+    '',
+    '^[./]'
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+  plugins: []
+}
